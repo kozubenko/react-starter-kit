e88914bbeba34c63349c10bdd7274b6192677ee5
kozubenko <andrii.kozubenko@gmail.com>
2017-12-14 16:39:05 +0200

    initial commit

---
+++ src/componsents/Column/Column.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Column extends Component {
+    render() {
+        return (
+            <div>
+                Column
+            </div>
+        );
+    }
+}
---
+++ src/containers/App/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import styles from './styles.css';
+
+export default class App extends Component {
+  render() {
+    return (
+        <div className={styles.main}/>
+    );
+  }
+}
+
